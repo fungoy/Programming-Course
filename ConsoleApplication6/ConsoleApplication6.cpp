28d1137167c488d8a2245c1edaaa4ced24b57c61
camlinstead <camlinstead@msn.com>
2015-01-26 18:47:48 -0700

    Update ConsoleApplication6.cpp

--- ConsoleApplication6/ConsoleApplication6.cpp
+++ ConsoleApplication6/ConsoleApplication6.cpp
@@ -5,21 +5,55 @@
 #include <iostream>
 using namespace std;
 
+static void temperature ();
+static void asciitable();
+
+static void asciitable()
+{
+	for(int i = 65; i <= 122; i++)
+	{
+		if(i < 91 || i > 96)
+		{
+				cout << char(i) <<" ";
+		}
+		
+	}
+
+
+
+}
+
+
+static void temperature ()
+{
+	int clow;
+	int chigh; 
+
+	double ans;
+
+	cout << "Enter the low fahrenheit temperature: ";
+	cin >> clow;
+
+	cout << "Enter the high fahrenheit temperature: ";
+	cin >> chigh;
+
+	for(int i=clow; i <= chigh; i++)
+	{
+
+		ans = (i - 32.0)*5.0/9.0;
+		cout << "Answer is: " << ans << endl;
+
+	}
+
+
+}
+
 int _tmain(int argc, _TCHAR* argv[])
 {
-	int celsius;
-	cout << "Enter the temperature in celsius: ";
-	cin >> celsius;
+	//temperature();
+	asciitable(); 
 
-	int factor;
-	factor = 212 - 32;
-
-	int fahrenheit;
-	fahrenheit = factor*celsius / 100 + 32;
-
-	cout << "Fahrenheit value is: ";
-	cout << fahrenheit << endl;
 	system("PAUSE");
-	
+
 	return 0;
 }
